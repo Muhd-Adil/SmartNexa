6ab2a93908d33be4361fb5f1ffbd328f01f63551
Your Name <you@example.com>
2024-11-06 12:58:52 +0530

    Update Product table & Inventory Table

--- SmartNexa/seller/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import SellerRegisterView
-
-
-urlpatterns=[
-    path('/register',SellerRegisterView.as_view(),name="seller-register")
-]
-
--- SmartNexa/sn_user/serializers.py
+++ SmartNexa/sn_user/serializers.py
@@ -2,17 +2,18 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.tokens import RefreshToken
 
-User=get_user_model()
+User = get_user_model()
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['username','firstname','lastname','email','password','mobile']
+        fields = ['firstname', 'lastname', 'email', 'password', 'mobile']
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            username=validated_data['username'],
             firstname=validated_data['firstname'],
             lastname=validated_data['lastname'],
             email=validated_data['email'],
@@ -21,10 +22,12 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
         return user
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username','email', 'firstname', 'lastname','mobile']
+        fields = ['username', 'email', 'firstname', 'lastname', 'mobile']
+
 
 class JWTTokenSerializer(serializers.Serializer):
     token = serializers.CharField()
